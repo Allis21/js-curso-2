29dda7d2859274af90cd03037a8ea1c85242174e
Allis21 <134411551+Allis21@users.noreply.github.com>
2024-07-12 23:17:19 -0500

    finish challenge

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
+let cambioH1 = document.querySelector('h1');
+cambioH1.innerHTML = 'Hora del Desafío';
 
+function clickConsole() {
+    console.log('El botón fue clicado');
+}
+
+function clickPrompt() {
+    let ciudad = prompt('Ingrese una ciudad de Brasil');
+    alert(`Estuve en ${ciudad} y me acordé de ti`);
+}
+
+function clickAlerta() {
+    alert('Yo amo JS');
+}
+
+function clickSuma() {
+    let num1 = prompt('Ingrese un número: ');
+    let num2 = prompt('Ingrese otro número: ');
+    let suma = parseInt(num1) + parseInt(num2);
+    alert(`La suma de los números es: ${suma}`);
+}
